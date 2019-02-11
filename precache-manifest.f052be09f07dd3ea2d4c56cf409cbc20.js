28ca9b89d0212025ca9131711fceaca1b0d5267d
cafebabellc <47505851+cafebabellc@users.noreply.github.com>
2019-02-10 19:51:01 -0600

    Add files via upload

---
+++ precache-manifest.f052be09f07dd3ea2d4c56cf409cbc20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learning/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "179d924231aabc2b9a5c",
+    "url": "/learning/static/js/runtime~main.179d9242.js"
+  },
+  {
+    "revision": "9a1b91ad113b814684a2",
+    "url": "/learning/static/js/main.9a1b91ad.chunk.js"
+  },
+  {
+    "revision": "0f0f1ed395de9ee32ca5",
+    "url": "/learning/static/js/2.0f0f1ed3.chunk.js"
+  },
+  {
+    "revision": "9a1b91ad113b814684a2",
+    "url": "/learning/static/css/main.d9fd7f3f.chunk.css"
+  },
+  {
+    "revision": "316b1d2352587f0d608f49e467e950c1",
+    "url": "/learning/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e4cb419ac7ef4c54a7095aa6f58383c.js"
+  "/learning/precache-manifest.f052be09f07dd3ea2d4c56cf409cbc20.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/learning/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
