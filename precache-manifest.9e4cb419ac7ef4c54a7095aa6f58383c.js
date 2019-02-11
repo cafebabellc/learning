8460511ff43bec38e65e51b4a40aada0d8801a19
cafebabellc <47505851+cafebabellc@users.noreply.github.com>
2019-02-10 18:44:07 -0600

    Add files via upload

---
+++ precache-manifest.9e4cb419ac7ef4c54a7095aa6f58383c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1bc3f60ab79d537f3f8b",
+    "url": "/static/js/main.1bc3f60a.chunk.js"
+  },
+  {
+    "revision": "0f0f1ed395de9ee32ca5",
+    "url": "/static/js/2.0f0f1ed3.chunk.js"
+  },
+  {
+    "revision": "1bc3f60ab79d537f3f8b",
+    "url": "/static/css/main.d9fd7f3f.chunk.css"
+  },
+  {
+    "revision": "19e044c6cc44ad1afc9d2622e049f02e",
+    "url": "/index.html"
+  }
+];
